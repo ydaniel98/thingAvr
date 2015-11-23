76db7e0049bca9aa0d1dd42bd0dad0a12b8b911d
ydaniel98 <ed22333t@gmail.com>
2015-11-23 19:53:36 -0400

    Added in_outs file - contains macros for io pins

--- src/Pin.cpp
+++ src/Pin.cpp
@@ -4,34 +4,41 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "Pin.h"
-#include "convenience.h"
+#include "in_outs.h"
 
-Pin::Pin(PortData data, uint8_t pinNum): data(data), pinNum(pinNum) {}
+Pin::Pin(PortData data, uint8_t pinNum): pinNum(pinNum) {
+	ddr 	= data.ddr;
+	dPort	= data.dPort;
+	readR	= data.readR;
+}
+
 Pin::Pin(volatile uint8_t *ddr, volatile uint8_t *dPort, volatile uint8_t *readR, uint8_t pinNum): pinNum(pinNum) {
-	data = PortData(ddr, dPort, readR);
+	ddr 	= ddr;
+	dPort	= dPort;
+	readR	= readR;
 }
 
 void Pin::high() {
-	set(1);
+	HIGH(*dPort, pinNum);
 }
 
 void Pin::low() {
-	set(0);
+	LOW(*dPort, pinNum);
 }
 
 void Pin::toggle() {
-	*(data.dPort) ^= 1 << pinNum;
+	TOGGLE(*dPort, pinNum);
 }
 
 bool Pin::read() {
-	return *(data.readR) & (1 << pinNum);
+	return READ(*readR, pinNum);
 }
 
 void Pin::set(bool s) {
-	s ? *(data.dPort) |= 1 << pinNum : *(data.dPort) &= ~(1 << pinNum);
+	SET(*ddr, pinNum, s);
 }
 
 void Pin::mode(bool m) {
-	m ? *(data.ddr) |= 1 << pinNum : *(data.dPort) &= ~(1 << pinNum);
+	MODE(*ddr, pinNum, m);
 }
 #endif
