c65d27332325068b3fa5033146a64ea487a43458
ydaniel98 <ed22333t@gmail.com>
2015-12-16 23:20:30 -0400

    Moved C++ part to new folder Added debouncing function to io_s

--- C/src/io_s.h
+++ C/src/io_s.h
@@ -11,7 +11,39 @@
 
 #define SET(PORT, PIN, S) 	S ? HIGH(PORT, PIN) : LOW(PORT, PIN)
 
-#define READ(READ_R, PIN)	READ_R & (1 << PIN)
+#define READ(READ_R, PIN)	(READ_R & (1 << PIN))
+
+#ifdef BTN_READ_SAMPLES
+
+unsigned int btnLowSamples  = 0;
+unsigned int btnHighSamples = 0;
+
+uint8_t btnRead(volatile uint8_t readR, uint8_t pin) {
+	while (1) {
+		
+		if (READ(readR, pin)) 
+			btnHighSamples++;
+		else
+			btnHighSamples = 0;
+			
+		if (!READ(readR, pin))
+			btnLowSamples++;
+		else
+			btnLowSamples = 0;
+		
+		if (btnHighSamples == BTN_READ_SAMPLES) {
+			btnHighSamples = 0;
+			return 1;
+		}
+			
+		if (btnLowSamples == BTN_READ_SAMPLES) {
+			btnLowSamples = 0;
+			return 0;
+		}
+	}
+}
+
+#endif
 
 
 #endif
