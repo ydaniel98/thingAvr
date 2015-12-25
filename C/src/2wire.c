d72b545d8363de879e907feef7d4e4d95b934247
ydaniel98 <ed22333t@gmail.com>
2015-12-25 13:05:25 -0400

    Finished main functionality for TWI.

--- C/src/2wire.c
+++ C/src/2wire.c
@@ -12,7 +12,7 @@ uint8_t tWireTransmittion(uint8_t a) {
 			break;
 		case T_WIRE_STOP:
 			T_WIRE_SEND_STOP();					// Sends the stop condition
-			break;
+			return 1;
 		case T_WIRE_DATA:
 			T_WIRE_SEND_DATA();					// Sends the data in the tw data register
 	}
@@ -22,71 +22,60 @@ uint8_t tWireTransmittion(uint8_t a) {
 }
 
 uint8_t tWireWrite8(uint8_t data, uint8_t addr) {
-	if (!tWireGetSlaveWrite(addr))
-		return 0;
-	data = tWireWriteNS(data);
-	tWireTransmittion(T_WIRE_STOP);
-	return (data);
+	
+	uint8_t twStatus;
+	
+	T_WIRE_START_C(twStatus);
+	T_WIRE_GET_SLAVE_WRITE(addr, twStatus);
+	T_WIRE_WRITE_DATA(data, twStatus);
+
+	endS:
+		tWireTransmittion(T_WIRE_STOP);
+		return twStatus;
 }
 
-uint8_t tWireWriteNS(uint8_t data) {
-	TWDR = data;
-	return (tWireTransmittion(T_WIRE_DATA) == TW_MT_DATA_ACK);
-}
 
 uint8_t tWireWriteA(uint8_t * data, uint8_t addr, uint8_t s) {
-	if (!tWireGetSlaveWrite(addr))
-		return 0;
-	addr = 1;
+	
+	uint8_t twStatus;
+	
+	T_WIRE_START_C(twStatus);
+	T_WIRE_GET_SLAVE_WRITE(addr, twStatus);
+	
 	for (; s >= 1; s--) {
-		if (!tWireWriteNS(*data++)) {
-			addr = 0;
-			break;
-		}
+		T_WIRE_WRITE_DATA(*data++, twStatus);
 	}
-	tWireTransmittion(T_WIRE_STOP);
-	return addr;
-}
-
-uint8_t tWireGetSlaveWrite(uint8_t addr) {
-	uint8_t twStatus;
-
-	twStatus = tWireTransmittion(T_WIRE_START);
-
-	if ((twStatus == TW_MT_ARB_LOST) || ((twStatus == TW_MT_SLA_NACK) && (twStatus != TW_REP_START))) 
-		return 0;
-
-	TWDR = addr << 1;
-	twStatus = tWireTransmittion(T_WIRE_DATA);
-
-	return ((twStatus != TW_MT_SLA_NACK) || (twStatus != TW_MT_ARB_LOST) || (twStatus == TW_MT_SLA_ACK));
-}
-
-uint8_t tWireGetSlaveRead(uint8_t addr) {
-	uint8_t twStatus;
-
-	twStatus = tWireTransmittion(T_WIRE_START);
-
-	if ((twStatus == TW_MR_ARB_LOST) || ((twStatus == TW_MR_SLA_NACK) && (twStatus != TW_REP_START)))
-	return 0;
-
-	TWDR = addr << 1 | 1;
-	twStatus = tWireTransmittion(T_WIRE_DATA);
-
-	return ((twStatus != TW_MR_SLA_NACK) || (twStatus != TW_MR_ARB_LOST) || (twStatus == TW_MR_SLA_ACK));
+	
+	endS:
+		tWireTransmittion(T_WIRE_STOP);
+		return twStatus;
 }
 
 uint8_t tWireRead(uint8_t addr, uint8_t * buffer, uint16_t s) {
-	if (!tWireGetSlaveRead(addr))
-		return 0;
+	
+	uint8_t twStatus;
+	
+	T_WIRE_START_C(twStatus);
+	T_WIRE_GET_SLAVE_READ(addr, twStatus);
+	
 	for (; s >= 1; s--) {
+		
 		T_WIRE_READ_S();
 		T_WIRE_WAIT_TWINT();
+		
+		if (TWSR == TW_MR_DATA_NACK) {
+			twStatus = 0;
+			goto endS;
+		}
+			
 		*buffer++ = TWDR;
 	}
+	
 	TWCR = _BV(TWEN) | _BV(TWINT);
 	T_WIRE_WAIT_TWINT();
-	tWireTransmittion(T_WIRE_STOP);
-	return 1;
+
+	endS:
+		tWireTransmittion(T_WIRE_STOP);
+		return twStatus;
 }
 #endif
--- C/src/2wire.h
+++ C/src/2wire.h
@@ -21,13 +21,40 @@
 
 #define T_WIRE_GET_ID(ADDR, RW) ((ADDR << 1) | RW)
 
+#define T_WIRE_START_C(STATUS) \
+				STATUS = tWireTransmittion(T_WIRE_START); \
+				if ((STATUS == TW_MT_ARB_LOST) || ((STATUS == TW_MT_SLA_NACK) && (STATUS != TW_REP_START))) { \
+					STATUS = 0; \
+					goto endS; \
+				}
+
+#define	T_WIRE_GET_SLAVE_WRITE(ADDR, STATUS) \
+				TWDR = ADDR << 1 | TW_WRITE; \
+				STATUS = tWireTransmittion(T_WIRE_DATA); \
+				if (!((STATUS != TW_MT_SLA_NACK) || (STATUS != TW_MT_ARB_LOST) || (STATUS == TW_MT_SLA_ACK))) { \
+					STATUS = 0; \
+					goto endS; \
+				}
+					
+#define T_WIRE_WRITE_DATA(DATA, STATUS) \
+				TWDR = DATA; \
+				if (tWireTransmittion(T_WIRE_DATA) != TW_MT_DATA_ACK) { \
+					STATUS = 0; \
+					goto endS; \
+				}
+				
+#define T_WIRE_GET_SLAVE_READ(ADDR, STATUS) \
+				TWDR = ADDR << 1 | TW_READ; \
+				STATUS = tWireTransmittion(T_WIRE_DATA); \
+				if (!((STATUS != TW_MR_SLA_NACK) || (STATUS != TW_MR_ARB_LOST) || (STATUS == TW_MR_SLA_ACK))) { \
+					STATUS = 0; \
+					goto endS; \
+				}
+			
+
 uint8_t tWireTransmittion(uint8_t a);
 uint8_t tWireWrite8(uint8_t data, uint8_t addr);
 
-uint8_t tWireGetSlaveWrite(uint8_t addr);
-uint8_t tWireGetSlaveRead(uint8_t addr);
-
-uint8_t tWireWriteNS(uint8_t data);									// Sends byte with not stop condition and no address
 uint8_t tWireWriteA(uint8_t * data, uint8_t addr, uint8_t s);		// Sends all the bytes in the pointer
 
 uint8_t tWireRead(uint8_t addr, uint8_t * buffer, uint16_t s);		// Gets data and sets it on the buffer
